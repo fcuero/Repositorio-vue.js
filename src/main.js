e9d71633ea9de52f8ecd7c8f3bb249bd99241ba9
fcuero <138230730+fcuero@users.noreply.github.com>
2023-07-20 13:27:33 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,48 +3,12 @@
 
 import Vue from 'vue'
 import App from './App'
-/*
-import VueResource from 'vue-resource';
-Vue.use(VueResource);
-
-import VueRouter from 'vue-router';
-Vue.use(VueRouter);
- 
-import test from './components/test.vue';
-import User from './components/user.vue';
-
-
-const router = new VueRouter({
-  mode: 'history',
-  base: __dirname,
-  routes: [
-    {
-      path:'/',
-      components: User
-    },
-    {
-      path:'/test',
-      components: test
-
-    }
-   
-  ]
-})
-*/
-
-
 
 Vue.config.productionTip = false
-/* eslint-disable no-new */
 new Vue({
-
   el: '#app',
-  
-
   template: '<App/>',
   components: { App }
   
 })
-/*
-.$mount('#app')
-*/
+
